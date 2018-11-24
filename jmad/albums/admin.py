ec1ad5371805aa2ef2d905fe12a1d664686cf087
Siegel <csiegel@dmu.edu>
2018-11-24 13:50:40 -0600

    Adds str, ordering, and columns for the Track admin

--- jmad/albums/admin.py
+++ jmad/albums/admin.py
@@ -4,5 +4,9 @@ from .models import Album, Track
 
 # Register your models here.
 
+class TrackAdmin(admin.ModelAdmin):
+    model = Track
+    list_display = ('album', 'name', 'track_number')
+
 admin.site.register(Album)
-admin.site.register(Track)
+admin.site.register(Track, TrackAdmin)
